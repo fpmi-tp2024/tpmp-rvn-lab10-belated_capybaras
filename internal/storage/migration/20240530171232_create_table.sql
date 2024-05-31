ff1fe3d48389766dc21d4e4cef1b80df5243a5a3
maxim <maximbaranovskiy26042005@gmail.com>
2024-05-31 04:03:01 +0300

    feat: implemented work with photos

--- internal/storage/migration/20240530171232_create_table.sql
+++ internal/storage/migration/20240530171232_create_table.sql
@@ -1,35 +1,35 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
-    username TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    location_city TEXT,
-    description TEXT,
-    photo BYTEA
+                                     username TEXT NOT NULL,
+                                     email TEXT UNIQUE NOT NULL,
+                                     password TEXT NOT NULL,
+                                     location_city TEXT DEFAULT '',
+                                     description TEXT DEFAULT '',
+                                     photo BYTEA
 );
 
 CREATE TABLE IF NOT EXISTS shelters(
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    name TEXT,
-    location_city TEXT,
-    bill TEXT,
-    description TEXT,
-    photo BYTEA
+                                       username TEXT NOT NULL,
+                                       password TEXT NOT NULL,
+                                       email TEXT UNIQUE NOT NULL,
+                                       name TEXT Default '',
+                                       location_city TEXT Default '',
+                                       bill TEXT Default '',
+                                       description TEXT Default '',
+                                       photo BYTEA
 );
 
 CREATE TABLE IF NOT EXISTS dogs(
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INT,
-    weight DECIMAL,
-    photo BYTEA,
-    description TEXT,
-    short_description TEXT,
-    shelter_email TEXT NOT NULL REFERENCES shelters(email)
-);
+                                   id SERIAL PRIMARY KEY,
+                                   name TEXT NOT NULL,
+                                   age TEXT Default '',
+                                   weight TEXT Default '',
+                                   photo BYTEA,
+                                   description TEXT Default '',
+                                   short_description TEXT Default '',
+                                   shelter_email TEXT NOT NULL REFERENCES shelters(email)
+    );
 -- +goose StatementEnd
 
 -- +goose Down
